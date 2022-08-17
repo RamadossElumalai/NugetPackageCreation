5941c18aeac5f3c6f2600ce2212229ccd35c566a
Ramadoss Elumalai <ramadosse2264@gmail.com>
2022-08-17 10:57:22 -0400

    first commit

---
+++ Logger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ApplicationLogger
+{
+    public class Logger
+    {
+        /// <summary>
+        /// This Method is used to log message in the console
+        /// </summary>
+        /// <param name="message"></param>
+        public void LogMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
